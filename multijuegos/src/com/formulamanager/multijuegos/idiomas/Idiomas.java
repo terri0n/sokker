815f1247a672016a179a508c809a5990dee296e2
Terrion <63014185+terri0n@users.noreply.github.com>
2024-03-17 14:24:14 +0100

    Delete multijuegos directory

--- multijuegos/src/com/formulamanager/multijuegos/idiomas/Idiomas.java
+++
@@ -1,5 +0,0 @@
-package com.formulamanager.multijuegos.idiomas;
-
-public interface Idiomas {
-	public final static String APPLICATION_RESOURCES = Idiomas.class.getPackageName() + ".ApplicationResources";
-}
