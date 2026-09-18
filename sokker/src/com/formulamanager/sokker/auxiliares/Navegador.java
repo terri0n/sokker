11207f40fbd20c71e0610b1c09b4c4936e91cd25
Terrion <63014185+terri0n@users.noreply.github.com>
2026-09-18 08:54:18 +0100

    Route legacy training XML through JSON adapter

--- sokker/src/com/formulamanager/sokker/auxiliares/Navegador.java
+++ sokker/src/com/formulamanager/sokker/auxiliares/Navegador.java
@@ -117,6 +117,11 @@ public abstract class Navegador {
 
 	// Intenta acceder a una url hasta 3 veces
 	public static XmlPage getXmlPage(WebClient navegador, String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		XmlPage paginaJson = SokkerXmlCompat.getXmlPage(navegador, url);
+		if (paginaJson != null) {
+			return paginaJson;
+		}
+
 		int intentos = 0;
 		Exception ex;
 		
