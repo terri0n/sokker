2dcbb59c21de4e30d29aa4d73f2f373fc0d9cb66
Terrion <63014185+terri0n@users.noreply.github.com>
2026-09-18 18:52:52 +0100

    Authenticate Sokker API without HtmlUnit

--- sokker/src/com/formulamanager/sokker/auxiliares/Navegador.java
+++ sokker/src/com/formulamanager/sokker/auxiliares/Navegador.java
@@ -2,7 +2,6 @@ package com.formulamanager.sokker.auxiliares;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.ParseException;
 import java.util.LinkedHashMap;
 
@@ -15,10 +14,8 @@ import com.formulamanager.sokker.bo.AsistenteBO;
 import com.formulamanager.sokker.bo.FactorxBO;
 import com.formulamanager.sokker.entity.Usuario;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.xml.XmlPage;
 
 public abstract class Navegador {
@@ -67,15 +64,9 @@ public abstract class Navegador {
 	public WebClient hacer_login(String login, String password) throws FailingHttpStatusCodeException, MalformedURLException, IOException, LoginException {
 		WebClient navegador = crear_navegador();
 
-		//Proceso de login.
-		URL url = new URL(AsistenteBO.SOKKER_URL + "/api/auth/login");
-		WebRequest requestSettings = new WebRequest(url, HttpMethod.POST);
-		requestSettings.setRequestBody("{\"login\" : \"" + login + "\", \"password\" : \"" + password + "\", \"remember\" : false }");
-		requestSettings.setAdditionalHeader("Content-Type", "application/json; charset=utf-8");
 		try {
-			Page paginaLogin = navegador.getPage(requestSettings);
-//System.out.println(paginaLogin.getWebResponse().getContentAsString());
-		} catch (FailingHttpStatusCodeException e) {
+			SokkerApiHttp.login(navegador, AsistenteBO.SOKKER_URL + "/api/auth/login", login, password);
+		} catch (SokkerApiHttp.HttpStatusException e) {
 			navegador.close();
 			if (e.getStatusCode() == 401) {
 				throw new LoginExceptionExt("Error when logging in to Sokker: bad password", login, password);
