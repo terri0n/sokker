790186717a9e6b98de88f8fd120d418f15776fe5
Terrion <63014185+terri0n@users.noreply.github.com>
2026-09-24 16:26:35 +0100

    feat: support HTML deletion request email

--- sokker/src/com/formulamanager/sokker/auxiliares/EmailSenderService.java
+++ sokker/src/com/formulamanager/sokker/auxiliares/EmailSenderService.java
@@ -1,4 +1,5 @@
 package com.formulamanager.sokker.auxiliares;
+
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -19,69 +20,83 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
- 
+
 public class EmailSenderService {
-	private static String SMTP = "smtp.mailgun.org";
-	
-	private static final Properties properties = new Properties();
-	private static Session session;
- 
-	private static void init() {
- 		properties.put("mail.smtp.host", SMTP);
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.port", 587);
-		properties.put("mail.smtp.user", SystemUtil.getVar("mailgun_login"));
-		properties.put("mail.smtp.auth", "true");
-		
-		properties.put("mail.debug", "true");
-		properties.put("mail.smtps.localhost", "localhost");
- 
-		session = Session.getDefaultInstance(properties);
-	}
- 
-	public static void sendEmail(String email, String asunto, String texto, HashMap<String, byte[]> listaArchivos) throws AddressException, MessagingException, UnsupportedEncodingException {
- 		init();
+    private static final String SMTP = "smtp.mailgun.org";
+    private static final Properties properties = new Properties();
+    private static Session session;
 
-		MimeMessage message = new MimeMessage(session);
-		message.setFrom(new InternetAddress((String)properties.get("mail.smtp.user"), "Sokker Asistente"));
-		message.setSender(new InternetAddress((String)properties.get("mail.smtp.user"), "Sokker Asistente"));
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-		message.setSubject(asunto);
-		message.setText(texto);
-		
-		if (listaArchivos != null) {
-			// Para a�adir el texto
-			Multipart multipart = new MimeMultipart();
-			BodyPart htmlPart = new MimeBodyPart();
-			htmlPart.setContent(texto,"text/html");
-			htmlPart.setDisposition(BodyPart.INLINE);
-			multipart.addBodyPart(htmlPart);
-	
-			// A�adimos los archivos
-			Iterator<Entry<String, byte[]>> it = listaArchivos.entrySet().iterator();
-			while(it.hasNext()){
-				@SuppressWarnings("rawtypes")
-				Map.Entry e = (Map.Entry)it.next();
-				String mimeType = MimeType.getMimeType(devExtension(e.getKey().toString()));					
-				
-				BodyPart messageBodyPart = new MimeBodyPart();
-				messageBodyPart.setDataHandler(
-					new DataHandler(new ByteArrayDataSource((byte[])e.getValue(), mimeType))
-				);
-				messageBodyPart.setFileName(e.getKey().toString());
-				multipart.addBodyPart(messageBodyPart);
-			}
-			
-			message.setContent(multipart);
-		}		
+    private static void init() {
+        properties.put("mail.smtp.host", SMTP);
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.port", 587);
+        properties.put("mail.smtp.user", SystemUtil.getVar("mailgun_login"));
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.debug", "false");
+        properties.put("mail.smtps.localhost", "localhost");
+        session = Session.getDefaultInstance(properties);
+    }
 
-		Transport t = session.getTransport("smtp");
-		t.connect(SMTP, (String)properties.get("mail.smtp.user"), SystemUtil.getVar("mailgun_password"));
-		t.sendMessage(message, message.getAllRecipients());
-		t.close();
-	}
+    public static void sendEmail(String email, String asunto, String texto,
+            HashMap<String, byte[]> listaArchivos)
+            throws AddressException, MessagingException, UnsupportedEncodingException {
+        send(email, asunto, texto, listaArchivos, listaArchivos != null);
+    }
 
-	public static String devExtension(String nombre){
-		return nombre.substring(nombre.lastIndexOf(".") + 1);
-	}
+    public static void sendHtmlEmail(String email, String asunto, String html)
+            throws AddressException, MessagingException, UnsupportedEncodingException {
+        send(email, asunto, html, null, true);
+    }
+
+    private static void send(String email, String asunto, String texto,
+            HashMap<String, byte[]> listaArchivos, boolean html)
+            throws AddressException, MessagingException, UnsupportedEncodingException {
+        init();
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress((String) properties.get("mail.smtp.user"), "Sokker Asistente"));
+        message.setSender(new InternetAddress((String) properties.get("mail.smtp.user"), "Sokker Asistente"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+        message.setSubject(asunto, "UTF-8");
+
+        if (listaArchivos == null) {
+            if (html) {
+                message.setContent(texto, "text/html; charset=UTF-8");
+            } else {
+                message.setText(texto, "UTF-8");
+            }
+        } else {
+            Multipart multipart = new MimeMultipart();
+            BodyPart textPart = new MimeBodyPart();
+            if (html) {
+                textPart.setContent(texto, "text/html; charset=UTF-8");
+            } else {
+                textPart.setText(texto);
+            }
+            textPart.setDisposition(BodyPart.INLINE);
+            multipart.addBodyPart(textPart);
+
+            Iterator<Entry<String, byte[]>> it = listaArchivos.entrySet().iterator();
+            while (it.hasNext()) {
+                @SuppressWarnings("rawtypes")
+                Map.Entry entry = (Map.Entry) it.next();
+                String mimeType = MimeType.getMimeType(devExtension(entry.getKey().toString()));
+                BodyPart attachment = new MimeBodyPart();
+                attachment.setDataHandler(new DataHandler(
+                        new ByteArrayDataSource((byte[]) entry.getValue(), mimeType)));
+                attachment.setFileName(entry.getKey().toString());
+                multipart.addBodyPart(attachment);
+            }
+            message.setContent(multipart);
+        }
+
+        Transport transport = session.getTransport("smtp");
+        transport.connect(SMTP, (String) properties.get("mail.smtp.user"), SystemUtil.getVar("mailgun_password"));
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+
+    public static String devExtension(String nombre) {
+        return nombre.substring(nombre.lastIndexOf(".") + 1);
+    }
 }
